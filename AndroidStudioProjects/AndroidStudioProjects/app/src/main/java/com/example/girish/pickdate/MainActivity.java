1859171f1107e62f5776bd2f32e3dbf19d93e109
SireeshaPandala <pandalasireesha@gmail.com>
2018-05-08 23:17:56 -0500

    Adding ICP 7

---
+++ AndroidStudioProjects/AndroidStudioProjects/app/src/main/java/com/example/girish/pickdate/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.girish.pickdate;
+
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datepicker = new DialogPicker();
+                datepicker.show(getFragmentManager(),"date picker");
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR,year);
+        calendar.set(Calendar.MONTH,month);
+        calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+        String datestring = DateFormat.getDateInstance().format(calendar.getTime());
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText(datestring);
+    }
+}
